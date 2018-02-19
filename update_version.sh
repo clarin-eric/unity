71f8580ce70315d7bf11ab51e0336f81fd4b72bf
Willem Elbers <wjm.elbers@gmail.com>
2018-02-19 15:14:35 +0100

    added script to bump version

---
+++ update_version.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+#
+# Version structure:
+# <unity version>-<tag>-<clarin version>
+#
+# <unity version>	Main unity version this release is based off
+# <tag>			Tag with a static value of "clarin" to  indicate this is a clarin specific release
+# <clarin version>	Clarin release specific version, 
+#				_alpha#, _beta# and _rc# postfixes can be added to specify alpha, beta or 
+#				release candidate releases where # is replaced by a sequence number
+#
+
+CURRENT_VERSION="1.9.6-clarin-1.0.0"
+NEW_VERSION="1.9.6-clarin-1.0.0_beta1"
+
+echo "Updating all versions from ${CURRENT_VERSION} to ${NEW_VERSION}"
+find . -name pom.xml -exec sed -i '' "s/<version>${CURRENT_VERSION}<\/version/<version>${NEW_VERSION}<\/version>/g" {} \;
